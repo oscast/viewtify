4028d96ffb1ed83590d1e38b1b81a8428cf4f13f
Oscar Castillo <goldoac@live.com>
2020-12-15 16:29:46 -0600

    Initial project
    Initial swiftUI Project iOS 14.1

---
+++ Viewtify/ViewtifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewtifyApp.swift
+//  Viewtify
+//
+//  Created by Oscar Castillo on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewtifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
